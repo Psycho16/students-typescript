62072bf674bff156b3180e24c11cead2b4e2cbc1
PsychoXgod <Lobanov2000k@yandex.ru>
2021-06-05 09:39:54 +0500

    add theme for application

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'styled-components';
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primary: string;
+      secondary: string;
+    };
+    media: {
+      phone: string;
+    };
+  }
+}
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#000000',
+    secondary: '#49C2E8',
+  },
+  media: {
+    phone: '(max-width: 600px)',
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
