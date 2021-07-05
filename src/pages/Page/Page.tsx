9d6bb64bacd74b05efc8b6404c90f29f9169127b
PsychoXgod <Lobanov2000k@yandex.ru>
2021-07-05 08:50:14 +0500

    move filter and sort to store

--- src/pages/Page/Page.tsx
+++ src/pages/Page/Page.tsx
@@ -15,43 +15,9 @@ import {
 } from './StyledApp';
 
 import { Students, Search } from '../../components';
-
-import { getAgeFromBirthday } from '../../functions/getStudentInfo';
 import { onLoadStudents } from '../../functions/getData';
 
-interface student {
-  id: number;
-  email: string;
-  name: string;
-  sex: string;
-  specialty: string;
-  group: string;
-  color: string;
-  rating: number;
-  birthday: string;
-  avatar: string;
-}
-
 const Page: React.FC = () => {
-  function sortBy(a: student, b: student, type: string): number {
-    switch (type) {
-      case 'Имя А-Я':
-        return a.name.localeCompare(b.name);
-      case 'Имя Я-А':
-        return b.name.localeCompare(a.name);
-      case 'Сначала моложе':
-        return getAgeFromBirthday(a.birthday) - getAgeFromBirthday(b.birthday);
-      case 'Сначала старше':
-        return getAgeFromBirthday(b.birthday) - getAgeFromBirthday(a.birthday);
-      case 'Низкий рейтинг':
-        return a.rating - b.rating;
-      case 'Высокий рейтинг':
-        return b.rating - a.rating;
-      default:
-        return 0;
-    }
-  }
-
   React.useEffect(() => {
     onLoadStudents();
   }, []);
@@ -68,11 +34,8 @@ const Page: React.FC = () => {
         <StudentRating>Рейтинг</StudentRating>
       </StudentWrapper>
       <MainStudentsWrapper>
-        {store.students &&
-          store.students
-            .filter((student) => student.name.toLowerCase().includes(store.input.toLowerCase()))
-            .sort((a, b) => sortBy(a, b, store.sortType))
-            .map((student) => <Students key={student.id} {...student} />)}
+        {store.studentsForView &&
+          store.studentsForView.map((student) => <Students key={student.id} {...student} />)}
       </MainStudentsWrapper>
     </MainContent>
   );
--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { makeAutoObservable } from 'mobx';
+import { sortBy } from './functions/manipulateStudents';
 
 export interface student {
   id: number;
@@ -12,12 +13,24 @@ export interface student {
   birthday: string;
   avatar: string;
 }
+
 export const deleteStudent = (students: student[], id: number) => {
   return students.map((a) => a).filter((student) => student.id !== id);
 };
 
+export const filterStudents = (students: student[], input: string) => {
+  return students
+    .slice(0)
+    .filter((student) => student.name.toLowerCase().includes(input.toLowerCase()));
+};
+
+export const sortStudents = (students: student[], sortType: string) => {
+  return students.slice(0).sort((a, b) => sortBy(a, b, sortType));
+};
+
 class Store {
   students: student[] = [];
+  studentsForView: student[] = [];
   input: string = '';
   sortType: string = 'Имя А-Я';
 
@@ -26,14 +39,20 @@ class Store {
   }
   deleteStudent(id: number) {
     this.students = deleteStudent(this.students, id);
+    this.studentsForView = filterStudents(this.students, this.input);
+    this.studentsForView = sortStudents(this.studentsForView, this.sortType);
   }
   getStudents(students: student[]) {
     this.students = students;
+    this.studentsForView = students;
   }
   setSortType(label: string) {
+    this.studentsForView = sortStudents(this.studentsForView, label);
     this.sortType = label;
   }
   setInput(input: string) {
+    this.studentsForView = filterStudents(this.students, input);
+    this.studentsForView = sortStudents(this.studentsForView, this.sortType);
     this.input = input;
   }
 }
